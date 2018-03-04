4f8ae7944926078f6ec250917f1ad13441714f33
James Hamm <jameshamm1995@gmail.com>
2018-03-04 15:21:50 +0000

    Added sourcing data sets from online. The next step is to separate the functions out into appropiate files, and see if the mountain of string passing can be replaced with something nicer like an Enum.

--- velkozify/data/downloader.py
+++ velkozify/data/downloader.py
@@ -5,6 +5,8 @@ import os
 
 from time import sleep
 from urllib import request
+from urllib.error import URLError
+
 
 LOCAL_STORAGE_FOLDER = ".data_sets"
 
@@ -47,7 +49,7 @@ def download(url, data_format=None):
     if data_format is not None:
         # Coerce the data
         if data_format == "json":
-            data = json.loads(data.decode('utf-8'))
+            data = json.loads(data)
         else:
             message = "Data format ({}) is not known".format(data_format)
             raise ValueError(message)
@@ -59,7 +61,7 @@ def download(url, data_format=None):
 def get_version_data():
     """Return the known versions."""
     version_url = "https://ddragon.leagueoflegends.com/api/versions.json"
-    version_data = json.loads(download(version_url))
+    version_data = download(version_url, data_format="json")
     return version_data
 
 
@@ -97,6 +99,20 @@ def _get_all_items_saved_location(patch, region):
     return os.path.join(LOCAL_STORAGE_FOLDER, patch, region, "item.json")
 
 
+FILE_LOCATIONS = {
+    "champion": _get_champion_saved_location,
+    "all_champions": _get_all_champions_saved_location,
+    "all_items": _get_all_items_saved_location}
+
+
+# NOTE: These use an insecure http connection.
+ONLINE_LOCATIONS = {
+    "champion": "http://ddragon.leagueoflegends.com/cdn/{}/data/{}/champion/{}.json",
+    "all_champions": "http://ddragon.leagueoflegends.com/cdn/{}/data/{}/champion.json",
+    "all_items": "http://ddragon.leagueoflegends.com/cdn/{}/data/{}/item.json"
+}
+
+
 class DataManager:
     """Provide data in a convenient form.
 
@@ -108,62 +124,100 @@ class DataManager:
         self.patch = get_latest_patch() if patch is None else patch
         self.region = "en_US"
 
-    def _get_saved_data(self, champion_name=None,
-                        all_champions=None, all_items=None):
+    def _get_saved_data(self, query_type, query):
         """Return data if it is saved locally.
 
-        Only one data set should be requested at a time.
-
-        TODO: The api for this could be improved.
-        ASSUMPTION: The file format is presumed to be json.
+        Otherwise raise a DataNotFound Error.
         """
-        # Check the query is for only one data set.
-        if sum(q is not None for q in
-               (champion_name, all_champions, all_items)) != 1:
-            message = "More than one data set was requested at once."
-            raise ValueError(message)
+        get_file_location = FILE_LOCATIONS[query_type]
 
-        if all_champions is not None:
-            query = "all_champions"
-            expected_file_location = _get_all_champions_saved_location(
-                patch=self.patch, region=self.region)
-        elif all_items is not None:
-            query = "all_items"
-            expected_file_location = _get_all_items_saved_location(
-                patch=self.patch, region=self.region)
+        if query_type == "champion":
+            # Unlike other query types, the champion name is needed.
+            # It is presumed the champion name has already been normalized.
+            expected_file_location = get_file_location(
+                query, patch=self.patch, region=self.region)
         else:
-            query = champion_name
-            expected_file_location = _get_champion_saved_location(
-                champion_name, patch=self.patch, region=self.region)
+            expected_file_location = get_file_location(
+                patch=self.patch, region=self.region)
 
         try:
             with open(expected_file_location, 'r') as file:
                 data = json.load(file)
         except IOError:
             message = "Could not locate {} at {}".format(
-                query, expected_file_location)
+                query_type, expected_file_location)
+            raise DataNotFound(message)
+        else:
+            return data
+
+    def _download_data(self, query_type, query):
+        """Return data if it is found online.
+
+        Otherwise raise a DataNotFound Error.
+        """
+        url = ONLINE_LOCATIONS[query_type]
+
+        if query_type == "champion":
+            # It is presumed the champion name has already been normalized.
+            url = url.format(self.patch, self.region, query)
+        else:
+            url = url.format(self.patch, self.region)
+
+        try:
+            data = download(url, data_format="json")
+        except URLError:
+            message = "Could not locate {} at {}".format(
+                query_type, url)
             raise DataNotFound(message)
         else:
             return data
 
     def get_data(self, champion_name=None,
                  all_champions=None, all_items=None):
-        """Get the data on a particular champion.
+        """Get the data on a particular champion, all champions, or
+        all items.
 
-        Return the source of the data, followed by the data."""
-        champion_name = normalize(champion_name)
+        Return the source of the data, followed by the data.
+        If the data is not available anywhere, a DataNotFound Error
+        will be raised.
+
+        ASSUMPTION: The file format for every data set is presumed to be json.
+
+        TODO: This function is messy to read, and uses strings everywhere.
+        It should be reworked if possible when more data sets are added.
+        """
+        number_of_requested_data_sets = 0
+        if champion_name is not None:
+            champion_name = normalize(champion_name)
+            query_type, query = "champion", champion_name
+            number_of_requested_data_sets += 1
+
+        if all_champions is not None:
+            query_type, query = "all_champions", "all_champions"
+            number_of_requested_data_sets += 1
+
+        if all_items is not None:
+            query_type, query = "all_items", "all_items"
+            number_of_requested_data_sets += 1
+
+        # Only one data set should be requested at once
+        if number_of_requested_data_sets != 1:
+            message = "More than one data set was requested at once."
+            raise ValueError(message)
 
         # Look locally for the data
         try:
-            data = self._get_saved_data(
-                champion_name, all_champions, all_items)
+            data = self._get_saved_data(query_type, query)
         except DataNotFound:
             pass
         else:
             return "locally", data
 
         # Find the data online
-        # NOTE: This uses an insecure http connection.
-        url = "http://ddragon.leagueoflegends.com/cdn/{}/data/{}/champion/{}.json"
-        url = url.format(self.patch, self.region, champion_name)
-        raise NotImplementedError("Can't fetch data online yet.")
+        try:
+            data = self._download_data(query_type, query)
+        except DataNotFound:
+            message = "Could not find {} locally or online.".format(query)
+            raise DataNotFound(message)
+        else:
+            return "online", data
